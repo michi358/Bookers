8cba7a6961ac89537904f8d5fb0df0b6c03afdfb
EC2 Default User <ec2-user@ip-172-31-2-144.ap-northeast-1.compute.internal>
2024-02-09 11:13:35 +0000

    [finish]新規投稿フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
+  get '/books' => 'books#index' #投稿一覧画面
   get '/new' => 'books#new' #投稿画面
   get '/show' => 'books#show' #投稿詳細画面
-  get '/index' => 'books#index' #投稿一覧画面
   get '/edit' => 'books#edit' #編集画面
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
