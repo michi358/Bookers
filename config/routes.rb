9c900b24236a6a1ebda8099da9b9245717e26cc6
EC2 Default User <ec2-user@ip-172-31-2-144.ap-northeast-1.compute.internal>
2024-02-12 06:19:04 +0000

    [finish]destroy機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get '/' => 'homes#top'
-  get '/books' => 'books#index'  #投稿一覧画面
-  post 'books' => 'books#create'
-  # get '/new' => 'books#new' #投稿画面
-  get '/show/:id' => 'books#show' , as:'show' #投稿詳細画面
-  get '/edit' => 'books#edit' #編集画面
+  resources :books
+  # get '/books' => 'books#index'  #投稿一覧画面
+  # post 'books' => 'books#create'
+  # # get '/new' => 'books#new' #投稿画面
+  # get '/:id' => 'books#show' , as:'show' #投稿詳細画面
+  # get '/:id/edit' => 'books#edit' , as:'edit' #編集画面
+  # patch '/:id' => 'books#update' , as:'update'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
