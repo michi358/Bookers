1b30af2ee12b066c0a1ea6533c2018bb9295882c
EC2 Default User <ec2-user@ip-172-31-2-144.ap-northeast-1.compute.internal>
2024-02-12 13:49:45 +0000

    [Add]updateのバリデーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to show_path(@book.id)
-    else render :index
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else 
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,14 +28,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @books = Book.find(params[:id])
+    if @books.update(book_params)
+      flash[:notice] = "Book was successfully update."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destoroyde."
     redirect_to books_path
     
   end
